c9df197bc972daad78d7a6e2ac73fb3a6371247c
crinamogorean <88611325+crinamogorean@users.noreply.github.com>
2021-08-08 14:27:33 +0300

    feat: added push to dashboard for delete note button (#16)

---
+++ src/components/DeleteButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <button class="delete-button" @click="goToDashboard()">Delete</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Delete",
+  data() {
+    return {};
+  },
+  methods: {
+    goToDashboard() {
+      this.$router.push("/dashboard");
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import Note from "./Note.vue";
+import Dashboard from "./Dashboard.vue";
 import { createWebHistory, createRouter } from "vue-router";
 
-const routes = [{ path: "/", component: App }];
+const routes = [
+  { path: "/", component: App },
+  { path: "/notes/:noteId", component: Note },
+  { path: "/dashboard", component: Dashboard }
+];
 
 const router = createRouter({
   history: createWebHistory(),
