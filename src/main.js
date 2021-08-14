7b7112fd6265e319f910d4b62b930b781764be3f
alexandrina12 <88610976+alexandrina12@users.noreply.github.com>
2021-08-14 15:51:02 +0300

    initial commit

--- src/main.js
+++ src/main.js
@@ -2,15 +2,17 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import Landing from "./Landing.vue";
 import Note from "./Note.vue";
+import Login from "./Login.vue";
 import Dashboard from "./Dashboard.vue";
 import { createWebHistory, createRouter } from "vue-router";
-import Register from "./pages/Register.vue";
+import Register from "./Register.vue";
 
 const routes = [
   { path: "/", component: Landing },
   { path: "/register", component: Register },
   { path: "/notes/:noteId", component: Note },
-  { path: "/dashboard", component: Dashboard }
+  { path: "/dashboard", component: Dashboard },
+  { path: "/login", component: Login }
 ];
 
 const router = createRouter({
