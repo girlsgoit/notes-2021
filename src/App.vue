6253a754e8fc269675ddff40412cff49364a397e
Cristian Petcu <waickem@gmail.com>
2021-08-14 22:11:57 +0300

    Header (#39)

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,48 @@
 <template>
+  <HeaderLogin v-if="token" :user="user" />
+  <HeaderLogout v-else />
+
   <router-view />
 </template>
 
 <script>
+import emitter from "@/services/emitter";
+import HeaderLogin from "./components/HeaderLogin";
+import HeaderLogout from "./components/HeaderLogin";
+
 export default {
   name: "App",
-  components: {}
+  components: {
+    HeaderLogin,
+    HeaderLogout
+  },
+  data() {
+    return {
+      token: "",
+      user: {}
+    };
+  },
+  created() {
+    emitter.on("auth", () => this.updateUserData());
+
+    this.updateUserData();
+  },
+  methods: {
+    updateUserData() {
+      console.log("Here");
+      this.token = localStorage.getItem("NOTES_AUTH");
+
+      if (this.token) {
+        this.user = {
+          username: localStorage.getItem("USER_NAME"),
+          fullName: localStorage.getItem("FULL_NAME"),
+          id: localStorage.getItem("USER_ID")
+        };
+      }
+
+      console.log(this.user);
+    }
+  }
 };
 </script>
 
