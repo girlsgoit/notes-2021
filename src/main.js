b37eb77aa5bc2866e489c6b7779c8ed159f4d4c8
Cristian Petcu <waickem@gmail.com>
2021-08-14 16:15:44 +0300

    feat: login-guard (#34)

--- src/main.js
+++ src/main.js
@@ -7,6 +7,16 @@ import Dashboard from "./Dashboard.vue";
 import { createWebHistory, createRouter } from "vue-router";
 import Register from "./Register.vue";
 
+function loginGuard(from, to, next) {
+  console.log("Guarding!");
+  const token = localStorage.getItem("NOTES_AUTH");
+  if (token) {
+    next();
+  } else {
+    next("/login");
+  }
+}
+
 const routes = [
   { path: "/", component: Landing },
   { path: "/register", component: Register },
@@ -19,5 +29,22 @@ const router = createRouter({
   history: createWebHistory(),
   routes
 });
+
+Axios.interceptors.request.use(
+  function (config) {
+    const token = localStorage.getItem("NOTES_AUTH");
+
+    if (token) {
+      config.headers["Authorization"] = "Token " + token;
+    } else {
+      router.push("/login");
+    }
+
+    return config;
+  },
+  function (error) {
+    console.log(error);
+  }
+);
 
 createApp(App).use(router).mount("#app");
